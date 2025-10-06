51b1a96c98ea9a48bb7b0f158524965868a8581d
gouthem22 <ita2331@krce.ac.in>
2025-10-06 13:48:17 +0530

    Create Check whether a triangle.java
    Get 3 numbers from the user , compare the numbers and print Equilateral or Isosceles or Scalene
    
    If all number are same - Equilateral
    If any two numbers are same - Isosceles
    If all the numbers are different - Scalene
    Hint : check the upper/lower case for the output
    
    Input Format
    
    Integer
    
    Constraints
    
    1 < n < 100
    
    Output Format
    
    Equilateral/Isosceles/Scalene
    
    Sample Input 0
    
    2 2 2
    Sample Output 0
    
    Equilateral
    Explanation 0
    
    all the numbers are same
    
    Sample Input 1
    
    4 5 4
    Sample Output 1
    
    Isosceles

---
+++ Check whether a triangle.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int n3=sc.nextInt();
+        if(n1==n2 && n1==n3){
+            System.out.println("Equilateral");
+        }
+        else if(n1==n2 || n1==n3){
+            System.out.println("Isosceles");
+        }
+        else{
+            System.out.println("Scalene");
+        }
+    }
+}
